24ed8b1e60ea21dcf88fdade34078bbdd617bcbe
Benjamin Otte <otte@redhat.com>
2011-02-18 19:56:39 +0100

    arc: Handle radius == 0 the same no matter the arc direction
    Commit a0ea0b63fdd38a73d6696da5cd4800d9a7289240 fixed cairo_arc() but
    did not apply the same patch to cairo_arc_negative(). This patch fixes
    that oversight.

--- src/cairo.c
+++ src/cairo.c
@@ -1896,7 +1896,12 @@ cairo_arc_negative (cairo_t *cr,
 	return;
 
     /* Do nothing, successfully, if radius is <= 0 */
-    if (radius <= 0.0)
+    if (radius <= 0.0) {
+	cairo_line_to (cr, xc, yc); /* might become a move_to */
+	cairo_line_to (cr, xc, yc);
+	return;
+    }
+
 	return;
 
     while (angle2 > angle1)
