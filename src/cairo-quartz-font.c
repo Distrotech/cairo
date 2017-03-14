bf6046aa6f8f695472ee8e4fd909ae12f6d41d62
Chen, Chihhan <aguaithefreak@gmail.com>
2017-03-15 01:40:51 +0800

    fix typo

--- src/cairo-quartz-font.c
+++ src/cairo-quartz-font.c
@@ -762,21 +762,34 @@ _cairo_quartz_scaled_glyph_init (void *abstract_font,
 
 static unsigned long
 _cairo_quartz_ucs4_to_index (void *abstract_font,
+        uint32_t ucs4)
+{
+    cairo_quartz_scaled_font_t *font = (cairo_quartz_scaled_font_t*) abstract_font;
+    cairo_quartz_font_face_t *ffont = _cairo_quartz_scaled_to_face(font);
+    CGGlyph glyph[2];
+    UniChar utf16[2];
+    
+    CFIndex len = CFStringGetSurrogatePairForLongCharacter (ucs4, utf16) ? 2 : 1;
+    CGFontGetGlyphsForUnicharsPtr (ffont->cgFont, utf16, glyph, len);
+
+    return glyph[0];
+}
+/*
+static unsigned long
+_cairo_quartz_ucs4_to_index (void *abstract_font,
 			     uint32_t ucs4)
 {
     cairo_quartz_scaled_font_t *font = (cairo_quartz_scaled_font_t*) abstract_font;
     cairo_quartz_font_face_t *ffont = _cairo_quartz_scaled_to_face(font);
 	CGGlyph glyph[2];
 	UniChar utf16[2];
-	CFIndex len = CFStringSurrogatePairForeCharacter(ucs4, utf16)? 2 : 1;
-	CGFontGetGlyphsForUnicharsPtr(ffont->cgFont, utf16, glyph, len);
-	return glyph[0];
-//    UniChar u = (UniChar) ucs4;
-//    CGGlyph glyph;
-//    CGFontGetGlyphsForUnicharsPtr (ffont->cgFont, &u, &glyph, 1);
-//    return glyph;
+    UniChar u = (UniChar) ucs4;
+    CGGlyph glyph;
+    CGFontGetGlyphsForUnicharsPtr (ffont->cgFont, &u, &glyph, 1);
+    return glyph;
 
 }
+*/
 
 static cairo_int_status_t
 _cairo_quartz_load_truetype_table (void	            *abstract_font,
